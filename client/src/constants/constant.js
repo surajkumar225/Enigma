d0fcc884225c3e5b7afac263b6cbc00f1389cb31
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2023-10-10 12:32:48 +0530

    Updated

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,2 +1,2 @@
 export const ducklogo =
-  "https://flyclipart.com/thumbs/duckduckgo-2-logo-transparent-logo-de-duck-duck-go-1147537.png";
+  "https://logos.flamingtext.com/Name-Logos/Enigma-design-china-name.png";
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+// const API_URL = "http://localhost:8080";
 
-const API_DMAIL = async (urlObject, payload) => {
-  return await axios({
-    method: "urlObject.method",
-    url: `${API_URL}/${urlObject.endpoint}`,
-    data: payload,
-  });
-};
+// const API_DMAIL = async (urlObject, payload) => {
+//   return await axios({
+//     method: "urlObject.method",
+//     url: `${API_URL}/${urlObject.endpoint}`,
+//     data: payload,
+//   });
+// };
 
-export default API_DMAIL;
+// export default API_DMAIL;
